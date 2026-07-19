33dcf850d89ec5b6ae781187a974d50262110cc9
omedjialfred99-create <omedjialfred99@gmail.com>
2026-07-19 15:00:55 +0100

    modification of the remain pages for coherance

--- student.js
+++ student.js
@@ -1,14 +1,15 @@
 const menuToggle = document.getElementById('menuToggle');
-    const sidebar = document.querySelector('.sidebar'); // Ajuste la classe si elle s'appelle autrement
+const sidebar = document.querySelector('.sidebar');
 
+if (menuToggle && sidebar) {
     menuToggle.addEventListener('click', (e) => {
+        e.stopPropagation();
         sidebar.classList.toggle('active');
-        e.stopPropagation(); // Évite que le clic se propage
     });
 
-    // Optionnel : Fermer le menu si on clique n'importe où en dehors de la sidebar
     document.addEventListener('click', (e) => {
         if (!sidebar.contains(e.target) && !menuToggle.contains(e.target)) {
             sidebar.classList.remove('active');
         }
     });
+}
